a21d7070d5de82347e651969d42aa805c6a1045c
imshn <shahnawaz28april@gmail.com>
2025-11-25 14:50:58 +0530

    redirect issue

--- next.config.ts
+++ next.config.ts
@@ -27,8 +27,12 @@ const nextConfig: NextConfig = {
             value: "max-age=63072000; includeSubDomains; preload",
           },
           {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+          {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://checkout.razorpay.com https://apis.google.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' blob: data: https://*.googleusercontent.com https://*.razorpay.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://api.openai.com https://lottie.host https://*.firebaseio.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com https://firebasestorage.googleapis.com; frame-src 'self' https://api.razorpay.com;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://checkout.razorpay.com https://apis.google.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' blob: data: https://*.googleusercontent.com https://*.razorpay.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://api.openai.com https://lottie.host https://*.firebaseio.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com https://firebasestorage.googleapis.com https://*.firebaseapp.com; frame-src 'self' https://api.razorpay.com https://*.firebaseapp.com;",
           },
         ],
       },
